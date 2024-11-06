33c96dd3b77ae21f28f7b170f527f5db14d04fa7
Jciro17 <cirojeronimo17@gmail.com>
2024-11-05 19:03:19 -0500

    Implementacion de las pruebas automatizadas

--- UcoBet-Generales/src/main/java/co/edu/uco/UcoBet/generales/init/UcoBetApplication.java
+++ UcoBet-Generales/src/main/java/co/edu/uco/UcoBet/generales/init/UcoBetApplication.java
@@ -1,6 +1,7 @@
 package co.edu.uco.UcoBet.generales.init;
 
 import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,11 +9,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.azure.security.keyvault.secrets.SecretClient;
-
 @SpringBootApplication
 @EnableJpaRepositories(basePackages={"co.edu.uco.UcoBet"})
 @EntityScan(basePackages= {"co.edu.uco.UcoBet.generales.application.secondaryports.entity"})
-@ComponentScan(basePackages= {" co.edu.uco.UcoBet.generales"})
+@ComponentScan(basePackages= {"co.edu.uco.UcoBet.generales"})
 public class UcoBetApplication implements CommandLineRunner {
 
 	private final SecretClient secretClient;
@@ -22,14 +22,11 @@ public class UcoBetApplication implements CommandLineRunner {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(UcoBetApplication.class, args);
+	    SpringApplication.run(UcoBetApplication.class, args);
 	}
 
     @Override
     public void run(String... args) throws Exception {
-
+        // Lógica adicional si es necesario
     }
-
-	
-
 }
